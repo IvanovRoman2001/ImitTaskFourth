4a69bbb3413288f7440641e7e8311e471b6701cc
IvanovRoman2001 <ri338700@gmail.com>
2022-03-17 15:15:22 +0600

    added gitignore, 3 idz

--- src/main/java/com/imit/tasks/fourth/RationalFunction.java
+++ src/main/java/com/imit/tasks/fourth/RationalFunction.java
@@ -5,6 +5,7 @@ public class RationalFunction implements ISingleArgumentFunction {
     double right, left;
 
     public RationalFunction(double a, double b, double c, double d, double left, double right) {
+        if (c == 0 && d == 0) throw new IllegalArgumentException("Знаменатель не может равняться нулю");
         A = a;
         B = b;
         D = d;
@@ -15,7 +16,7 @@ public class RationalFunction implements ISingleArgumentFunction {
 
     @Override
     public double getValue(double x) throws IllegalArgumentException {
-        if (x > right || x < left) throw new IllegalArgumentException();
+        if (x > right || x < left || C * x + D == 0) throw new IllegalArgumentException();
         return (A * x + B) / (C * x + D);
     }
 
--- src/main/java/com/imit/tasks/fourth/SumFunctional.java
+++ src/main/java/com/imit/tasks/fourth/SumFunctional.java
@@ -1,18 +1,18 @@
 package com.imit.tasks.fourth;
 
 public class SumFunctional<T extends ISingleArgumentFunction> implements IFunctional<T> {
-    private final double right, left;
+    /*private final double right, left;
 
     public SumFunctional(double left, double right) {
         this.left = left;
         this.right = right;
-    }
+    }*/
 
     @Override
     public double calculate(T func) {
-        if (left < func.getLeft() || right > func.getRight()) throw new IllegalArgumentException();
+        //if (left < func.getLeft() || right > func.getRight()) throw new IllegalArgumentException();
         return  func.getValue(func.getRight()) +
                 func.getValue(func.getLeft()) +
-                func.getValue((func.getRight() + func.getRight()) / 2);
+                func.getValue((func.getLeft() + func.getRight()) / 2.0);
     }
 }
